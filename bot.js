209a616f19d30a07324d5f4cedc13afe8c7d01de
Trevor DeBardeleben <trevordebard@gmail.com>
2017-07-08 15:16:30 -0500

    Hopefully this works

--- bot.js
+++ bot.js
@@ -4,24 +4,19 @@ var cool = require('cool-ascii-faces');
 var botID = process.env.BOT_ID;
 
 function respond() {
-  var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^\/cool guy$/;
-
-  if(request.text && botRegex.test(request.text)) {
-    this.res.writeHead(200);
-    postMessage();
-    this.res.end();
-  } else {
-    console.log("don't care");
-    this.res.writeHead(200);
-    this.res.end();
-  }
+	var message = JSON.parse(this.req.chunks[0]);
+	if(message.text == "Venus, what are you") {
+		explainVenus();
+	}
 }
 
-function postMessage() {
-  var botResponse, options, body, botReq;
+function explainVenus() {
+	var response = "Oh, I'm just this thing Trevor created to be petty.\nI don't do shit rn tho.";
+	postMessage(response);
+}
 
-  botResponse = cool();
+function postMessage(botResponse) {
+  var options, body, botReq;
 
   options = {
     hostname: 'api.groupme.com',
@@ -39,19 +34,12 @@ function postMessage() {
   botReq = HTTPS.request(options, function(res) {
       if(res.statusCode == 202) {
         //neat
-      } else {
+      }
+      else{
         console.log('rejecting bad status code ' + res.statusCode);
       }
-  });
 
-  botReq.on('error', function(err) {
-    console.log('error posting message '  + JSON.stringify(err));
   });
-  botReq.on('timeout', function(err) {
-    console.log('timeout posting message '  + JSON.stringify(err));
-  });
-  botReq.end(JSON.stringify(body));
-}
 
 
 exports.respond = respond;
