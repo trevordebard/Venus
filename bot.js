645a29f2e94d008f33c6499886c01ade93f0410d
Trevor DeBardeleben <trevordebard@gmail.com>
2017-07-09 16:53:25 -0500

    pdf

--- bot.js
+++ bot.js
@@ -100,7 +100,7 @@ function analyzeGroup(){
 function sendDM() {
 	console.log("sendDmM call");
 	var memberId = getMemberId();
-	console.log("memberId: " + JSON.stringify(memberId));
+	console.log("memberId: " + JSON.stringify(memberId.data));
 }
 
 
