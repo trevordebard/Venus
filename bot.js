662689d12c89347d4fe1fd32e6b923a98a5c0077
Trevor DeBardeleben <trevordebard@gmail.com>
2017-07-09 20:50:35 -0500

    trying paths

--- bot.js
+++ bot.js
@@ -150,7 +150,7 @@ function sendDirectMessage(userId, message) {
 
   options = {
     hostname: 'api.groupme.com',
-    path: '/v3/direct_messages?token=UY5lfCVqEPlpQhge4UlydU6e6iQojUfmFPNCr2yB',
+    path: '/v3/direct_messages/'+userId+'?token=UY5lfCVqEPlpQhge4UlydU6e6iQojUfmFPNCr2yB',
     method: 'POST'
   };
 
