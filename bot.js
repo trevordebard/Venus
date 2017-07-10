c9b1163dbc92dcfb3010c1ba590eec1c2201c6e9
Trevor DeBardeleben <trevordebard@gmail.com>
2017-07-09 21:43:14 -0500

    dsfas

--- bot.js
+++ bot.js
@@ -154,7 +154,7 @@ function sendDirectMessage(userId, message) {
     method: 'POST'
   };
 
-  direct_message = {
+  "direct_message":{
     "source_guid": "5257bdd049240135837b22000b9ea932",
     "recipient_id": userId,
     "text": message
